e02626e69832d615cd38b956c0027e7a830454b2
Ann Porter <34779443+Jedi-Mom@users.noreply.github.com>
2018-03-29 19:27:02 -0400

    Update lb.js

--- js/lb.js
+++ js/lb.js
@@ -1,16 +1,10 @@
-// $('#flashInst')
-//   .hide()
-//   .fadeIn(3000)
-//   .delay(10000)
-//   .fadeOut();
-
-
 // When the button is pressed 
 $('.photoToggle').on('click', 'button', function(){
   //Show the edited text
   $(this).prev().show();
   //Hide the "Click" button
   $(this).hide();
+  .fadeIn(3000);
 });
 
 // Create the "Light Box Instruction" button
@@ -20,3 +14,10 @@ $('.photoToggle').append($button);
 
 //Hide the edited text
 $('.photoToggle span').hide();
+
+
+$('#flashInst')
+  .hide()
+  .fadeIn(3000)
+   .delay(10000)
+   .fadeOut();
